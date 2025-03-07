e1511adc2bd36660f73e39129f9eb058fe45dbcd
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-03-07 18:44:43 +0100

    3 (not working)

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,5 +14,28 @@ namespace GradeBook.GradeBooks
             Name = name;
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Too few students!");
+
+            int studentPlace = 1;
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                    studentPlace++;
+            }
+
+            if (studentPlace / Students.Count >= 0.2)
+                return 'A';
+            else if (studentPlace / Students.Count <= 0.2 && studentPlace / Students.Count > 0.4)
+                return 'B';
+            else if (studentPlace / Students.Count <= 0.4 && studentPlace / Students.Count > 0.6)
+                return 'C';
+            else if (studentPlace / Students.Count <= 0.6 && studentPlace / Students.Count > 0.8)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
