adf582a6f96d0f70e7cec134243a159369730828
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-03-07 18:02:58 +0100

    2

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
