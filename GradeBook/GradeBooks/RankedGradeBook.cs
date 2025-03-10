8a7782c9903324b4351c90a624be580295d9df3d
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-03-10 20:50:18 +0100

    update 5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,5 +34,12 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
     }
 }
